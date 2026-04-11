f215f3401137ff0b9b2384d2ff76d5a9d8ce78b5
4radea <aradeasatria18@gmail.com>
2026-04-11 23:27:12 +0700

    Add files via upload

---
+++ 07_Grammar-based Input Processing/TP/index.js
@@ -0,0 +1,17 @@
+function toNumberArray(input) {
+    let arr = [];
+
+    if (typeof input === "string") {
+        arr = input.split(",");
+    } else if (Array.isArray(input)) {
+        arr = input;
+    }
+    return arr
+    .map(item => item.toString().trim())
+    .map(item => parseFloat(item))
+    .filter(num => !isNaN(num));
+}
+console.log(toNumberArray("1, 2")) // [1, 2]
+console.log(toNumberArray(["1", "2"])) // [1, 2]
+console.log(toNumberArray(" 11,55,33   ")) // [11, 55, 33]
+console.log(toNumberArray(["0.2", "-11", "abc23"])) // [0.2, -11]
