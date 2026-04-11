630dee65c70ae28a1618dafe78305c80c41f2190
4radea <aradeasatria18@gmail.com>
2026-04-11 23:28:50 +0700

    Add files via upload

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/GUIDED/index.js
@@ -0,0 +1,22 @@
+function divide(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError("Salah tipe data. Harus bilangan bulat");
+    }
+    if (b === 0) {
+        throw new Error("Tidak boleh dibagi dengan nol");
+    }
+
+    const hasil = a / b;
+    if (hasil * b === a){
+        return hasil;
+    }
+    return hasil;
+}
+
+try {
+    console.log(
+        divide(10, 0)
+    );
+} catch (error) {
+    console.error(error);
+}
